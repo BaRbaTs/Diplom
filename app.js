4573d3705a077811f956fcad55056c23aec84aec
Илья <pribish66@mail.ru>
2022-05-12 17:28:52 +0300

    В app.js записал код для входа взял его с Heroku

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const express = require("express");
-   
+
 const app = express();
-   
+
+const PORT = process.env.PORT || 80
+
 // создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = express.urlencoded({extended: false});
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
   
 app.use("/", express.static('./public'));
+app.post("/", urlencodedParser, function (request, response) {
+if(request.body.login == "Toryn" && request.body.password == "32167")
+{
+console.log("Success!");
+return response.sendFile(__dirname + '/public/add_page.html');
+}
 
-app.post("/login", (request, response) => {
-    if(!request.content) return response.sendStatus(400);
-    console.log(request.content);
-    response.send(`${request.content.login} - ${request.content.password}`);
+else
+{
+console.log("Failure!");
+return response.sendStatus(400);
+}
+response.send();
 });
-   
-app.listen(3000, () => console.log("Сервер запущен..."));
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+app.listen(PORT, ()=>console.log("Сервер запущен..."));
